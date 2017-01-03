a2705e32522f79c5e574b89a9d4b37d47d84007e
Alex Gould <alexgould17@gmail.com>
2017-01-02 16:40:13 -0800

    Added audio functionality & made play/pause buttons work in basic fashion in the songlist area.

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,7 +1,8 @@
 (function() {
-	function AlbumCtrl(Fixtures) {
+	function AlbumCtrl(Fixtures, SongPlayer) {
 		this.albumData = Fixtures.getAlbum();
+		this.songPlayer = SongPlayer;
 	}
 	
-	angular.module('blocJams').controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
+	angular.module('blocJams').controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
 })();
